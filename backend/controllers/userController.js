724b2ec36a610af7d1bebf8b6729706c37f6c5f3
nikballal <nikhil.ballal1994@gmail.com>
2022-11-12 16:38:23 +1100

    register user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
 
 //@desc     Register a new user
 //@route    /api/users
@@ -12,7 +14,36 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Please enter all the fields");
   }
 
-  res.send("Register route");
+  //if all fields are complete, find if user already exists
+  const userExists = await User.findOne({ email: email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //Create user (mapping the userModel fields with the req.body)
+  const user = await User.create({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+
+  //if user is created
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //@desc     Login a user
