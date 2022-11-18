e4a8a88a3c0df14c4e32e1b40b4fb0129b7b73ea
nikballal <nikhil.ballal1994@gmail.com>
2022-11-18 14:04:45 +1100

    redux toolkit boilerplate

---
+++ frontend/src/features/auth/authService.js
@@ -0,0 +1,19 @@
+//to make a request to the backend API
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+  extraReducers: (builders) => {},
+});
+
+export default authSlice.reducer;
